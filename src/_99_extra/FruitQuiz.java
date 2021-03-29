6ae32faa913487f5fb7837359a8c93746130c5f4
root <root@10f929b7e2aa>
2021-03-29 20:27:49 +0000

    HideTheCreeper

--- src/_99_extra/FruitQuiz.java
+++ src/_99_extra/FruitQuiz.java
@@ -11,89 +11,85 @@ import javax.swing.JOptionPane;
 
 public class FruitQuiz extends KeyAdapter {
 
-	
 	JLabel question1 = new JLabel(), question2 = new JLabel(), question3 = new JLabel(), question4 = new JLabel(),
 			question5;
-	
+
 	void makeQuestions() {
-		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
-		// 11. Make another question called "question2".  Use question1 above as a guide.
-		question2 = new JLabel("<html>Which is not a real fruit <br> d: jalapeno <br> e: cucumber <br> f: tomato </html>");
 		question3 = new JLabel("<html>Which is a real fruit <br> g: jalapeno <br> h: celery <br> i: potato </html>");
+		question2 = new JLabel(
+				"<html>Which is not a real fruit <br> d: jalapeno <br> e: cucumber <br> f: tomato </html>");
+
+		question1 = new JLabel(
+				"<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
+		// 11. Make another question called "question2". Use question1 above as a guide.
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-System.out.println(keyCode);
+		System.out.println(keyCode);
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
 		int a = 65;
 		int b = 66;
 		int c = 67;
-		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
-		
-		
-		
-		// 12. If question2 is showing,
+		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The
+		// questions must be in reverse order from top to bottom to work properly
+		if (question1.isShowing()) {
+			if (keyCode == 67) {
+				JOptionPane.showMessageDialog(null, "Correct!");
+			} else {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+			}
+			nextQuestion(question2);
+		}
+		else if (question2.isShowing()) {
+			// 12. If question2 is showing,
 			int d = 68;
 			int e = 69;
 			int f = 70;
-			
+
 			if (keyCode == 69) {
 				JOptionPane.showMessageDialog(null, "Correct!");
-			}
-			else {
+			} else {
 				JOptionPane.showMessageDialog(null, "Incorrect");
 			}
-			
-			nextQuestion(question2);
-			
+		
+
+		nextQuestion(question3);
+		}
+		else if (question3.isShowing()) {
 			int g = 71;
 			int h = 72;
 			int i = 73;
-			
+
 			if (keyCode == 71) {
 				JOptionPane.showMessageDialog(null, "Correct");
-			}
-			else {
+			} else {
 				JOptionPane.showMessageDialog(null, "Incorrect");
 			}
-			// 13. check if it is right or wrong like you did for question1
-			nextQuestion(question3);
-			
-			
-		if (question1.isShowing()) {
-			// 3. If they selected the right fruit, do steps 4 and 7
-			if (keyCode == c) {
-		
-			
-				// 4. Call the correct() method
-				JOptionPane.showMessageDialog(null, "Correct!");
-				// 7. Use the nextQuestion() method to go to question2
-			
-			}
-			// 8. else (if they touched something else)
-			else {
-				JOptionPane.showMessageDialog(null, "Incorrect.");
-			}
-				// 9. Call the incorrect() method
-		
 		}
+		// 13. check if it is right or wrong like you did for question1
 
-	}
+		
+
+		}
+	
+
+	
 
 	private void correct() {
 		// 5. Find a sound for when they get a question right, and drag it into
-		// the 'extra' package. It must be a .wav file. 
+		// the 'extra' package. It must be a .wav file.
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-
 
 	}
 
 	private void incorrect() {
-		// 10. Find a sound for wrong answers and put it in the default package. Use the playSound method to play it.
+		// 10. Find a sound for wrong answers and put it in the default package. Use the
+		// playSound method to play it.
 
 	}
 
@@ -127,6 +123,4 @@ System.out.println(keyCode);
 		frame.addKeyListener(this);
 	}
 
-	
 }
-
--- src/_99_extra/hide_the_creeper/hide_the_creeper.pde
+++ src/_99_extra/hide_the_creeper/hide_the_creeper.pde
@@ -1,7 +1,36 @@
+PImage creeper;
+int creeperX = 489;
+int creeperY= 483;
 void setup() {
-  
+  size(500,500);
+  PImage ForestGreen = loadImage("ForestGreen.jpeg");
+  ForestGreen.resize(width,height);
+  background(ForestGreen);
+  creeper=loadImage("creeper.jpeg");
+  creeper.resize(10,20);
+   
 }
-
+//489 483
+//481 483
+//491 467
 void draw() {
   
+  image(creeper, creeperX, creeperY);
+  if(mousePressed){
+    println(mouseX + " " + mouseY);
+    fill(#D32929);
+    if (isNear()){
+      fill(#66BC55);
+    }
+     ellipse(mouseX, mouseY, 10, 10);
+  }
+ 
+}
+
+
+boolean isNear() {
+if (abs(creeperX+5 - mouseX) < 10  && abs(creeperY+10 - mouseY) < 10)
+     return true;
+else
+     return false;
 }
